0a98d57d947d18df3b175a4e7325d6f5434bcfb8
JamesDaniel <jamesmcgarr@protonmail.com>
2022-07-10 19:57:10 +0100

    Added error handling

--- src/app/service/weather.service.spec.ts
+++ src/app/service/weather.service.spec.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { waitForAsync, inject, TestBed, getTestBed } from '@angular/core/testing';
-import { WeatherResponse } from '../model/WeatherResponse';
+import { TestBed, getTestBed } from '@angular/core/testing';
 import { WeatherService } from './weather.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
@@ -35,6 +34,32 @@ describe('Test WeatherService', () => {
           fail();
         });
       httpMock.expectOne('https://api.openweathermap.org/data/2.5/forecast?q=Dublin,IE&units=metric&appid=377453ea6963542d2b4896b9ec7a9aca')
+    });
+
+    it('should handle client error', () => {
+      const alertSpy = spyOn(window, 'alert');
+      service.catchError({
+        error: {
+          message: 'client-side error'
+        }
+      });
+      expect(alertSpy).toHaveBeenCalledWith('client-side error');
+    });
+
+    it('should handle server client error', () => {
+      const alertSpy = spyOn(window, 'alert');
+      service.catchError({
+        message: 'server-side error'
+      });
+      expect(alertSpy).toHaveBeenCalledWith('server-side error');
+    });
+
+    it('should handle unknown error', () => {
+      const alertSpy = spyOn(window, 'alert');
+      service.catchError({
+        unknownError: 'unknown error'
+      });
+      expect(alertSpy).toHaveBeenCalledWith(JSON.stringify({unknownError:'unknown error'}));
     });
   });
 });
--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core'
+import { catchError, Observable, throwError } from 'rxjs';
 import { WeatherResponse } from '../model/WeatherResponse';
 
 @Injectable()
@@ -7,7 +8,20 @@ export class WeatherService {
 
     constructor(private http: HttpClient) {}
 
-    getFiveDayWeather(location: string) {
-      return this.http.get<WeatherResponse>(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=377453ea6963542d2b4896b9ec7a9aca`);
+    getFiveDayWeather(location: string): Observable<any> {
+      return this.http.get<WeatherResponse>(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=377453ea6963542d2b4896b9ec7a9aca`).pipe(
+        catchError(err => this.catchError(err))
+      );
+    }
+
+    catchError = (err: any) => {
+      if (err && err.error && err.error.message) {
+        alert(err.error.message);
+      } else if (err && err.message) {
+        alert(err.message);
+      } else {
+        alert(JSON.stringify(err));
+      }
+      return throwError(err);
     }
 }
